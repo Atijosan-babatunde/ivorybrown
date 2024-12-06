1d19e8651ebe3b542ea73f40e478faaafc6b2731
Babatunde-bepeerless <babatundeatijosan@bepeerless.co>
2024-12-06 16:44:17 +0100

    worked on the landing page

---
+++ public/mail/training.js
@@ -0,0 +1,50 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const trainingForm = document.getElementById('trainingForm');
+
+    trainingForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Get form values
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const subject = document.getElementById('subject').value;
+        const message = document.getElementById('message').value;
+
+        const sendMessageButton = document.getElementById('sendMessageButton');
+        sendMessageButton.disabled = true;
+
+        // Send data to the backend
+        axios.post('/mail', {
+            name,
+            email,
+            telephone,
+            message
+        })
+            .then(response => {
+                const successMessage = document.getElementById('success');
+                if (response.data.success) {
+                    successMessage.innerHTML = "<div class='alert alert-success'><strong>Your message has been sent. </strong></div>";
+                    trainingForm.reset();
+                } else {
+                    successMessage.innerHTML = `<div class='alert alert-danger'><strong>Sorry ${name}, there was an error sending your message. Please try again later!</strong></div>`;
+                }
+                sendMessageButton.disabled = false;
+            })
+            .catch(error => {
+                console.log(error);
+                const successMessage = document.getElementById('success');
+                successMessage.innerHTML = `<div class='alert alert-danger'><strong>Sorry ${name}, something went wrong. Please try again later!</strong></div>`;
+                sendMessageButton.disabled = false;
+            });
+    });
+
+    const inputs = document.querySelectorAll("#trainingForm input, #trainingForm textarea");
+    inputs.forEach(function (input) {
+        input.addEventListener('focus', function () {
+            document.getElementById('success').innerHTML = '';
+        });
+    });
+});
+
+
